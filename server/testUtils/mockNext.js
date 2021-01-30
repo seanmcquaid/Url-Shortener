2eaf46f615b31ae76835e76d048e69915a47a427
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-30 18:37:35 -0500

    removing tests

--- server/testUtils/mockNext.js
+++
@@ -1,5 +0,0 @@
-const sinon = require('sinon');
-
-const mockNext = () => sinon.spy();
-
-module.exports = mockNext;
--- server/testUtils/mockRequest.js
+++
@@ -1,8 +0,0 @@
-const sinon = require('sinon');
-
-const mockRequest = (body, params) => ({
-  body,
-  params,
-});
-
-module.exports = mockRequest;

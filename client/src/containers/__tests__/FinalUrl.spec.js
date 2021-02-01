310a83bc485f2b51a43d3c9497a6357dddabf4de
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-31 20:06:12 -0500

    added in unit test for characters saved

--- client/src/containers/__tests__/FinalUrl.spec.js
+++ client/src/containers/__tests__/FinalUrl.spec.js
@@ -21,4 +21,18 @@ describe('<FinalUrl/>', () => {
 
     expect(screen.getByText(`Old Url : fakeurl.com`)).toBeInTheDocument();
   });
+
+  it('Characters saved displays correctly', async () => {
+    renderWithContextAndRoute(
+      {
+        isLoading: false,
+        urlId: '3b7922cf-fda7-406e-b4a4-43d86e611cef',
+        originalUrl: 'fakeurl.com',
+        error: '',
+      },
+      '/finalUrl'
+    );
+
+    expect(screen.getByText(`Characters Saved : -35`)).toBeInTheDocument();
+  });
 });
